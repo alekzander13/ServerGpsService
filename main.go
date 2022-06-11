88f2a947ddef3f6d02eea5ebd38ad7c71efbb6be
alekzander13 <alekzander1313@gmail.com>
2022-06-11 15:24:59 +0300

    add data test parse

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"ServerGpsService/config"
+	"ServerGpsService/hash"
 	"ServerGpsService/mylog"
 	"ServerGpsService/utils"
 
@@ -23,7 +27,63 @@ func usage(errmsg string) {
 	os.Exit(2)
 }
 
+func testData() {
+	str := "000000000000003008010000018148888ef00010f5a4491dfcf22d00dc005a0f004900060301000200f000030900004236604310400000010000d6e2"
+	input, err := hex.DecodeString(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	must := []byte{0, 0, 0, 0}
+	have := make([]byte, 4)
+
+	all := make([]byte, len(input))
+	copy(all, input)
+
+	copy(have, input)
+
+	if !bytes.Equal(have, must) {
+		//if HEADER not 0000 = send bad request
+		log.Fatal("bad header " + string(have))
+	}
+
+	lenPacket, err := strconv.ParseInt(hex.EncodeToString(input[4:8]), 16, 64)
+	if err != nil {
+		log.Fatal("error parse length packet " + err.Error())
+	}
+
+	reqLen := len(input)
+	fmt.Printf("lenPack = %d; reqLen = %d\n", lenPacket, reqLen)
+
+	input = input[8:]
+
+	origByteCRC := input[lenPacket:]
+
+	input = input[:lenPacket]
+
+	fmt.Println(origByteCRC)
+	fmt.Println(hex.EncodeToString(origByteCRC))
+
+	origCRC, err := strconv.ParseUint(hex.EncodeToString(origByteCRC), 16, 64)
+	if err != nil {
+		log.Fatal("error parse crc packet " + err.Error())
+	}
+
+	dataCRC := hash.CheckSumCRC16(input)
+
+	fmt.Println(hex.EncodeToString(input))
+
+	fmt.Println("all =", hash.CheckSumCRC16(all[4:len(all)-4]))
+
+	if origCRC != uint64(dataCRC) {
+		log.Fatal(fmt.Sprintf("error crc sum: origCRC= %d, dataCRC= %d\n", origCRC, dataCRC))
+	}
+
+}
+
 func main() {
+	//testData()
+
 	if err := config.ReadConfig(utils.GetProgramPath() + ".json"); err != nil {
 		log.Fatal(err)
 	}
